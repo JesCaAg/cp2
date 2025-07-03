9899e0500ab087c18c390768eeffbddb0d3979fc
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-03 14:24:11 +0200

    Update rsa.tf
    Definicion descarga de la pem para posterior conexion a la vm

--- terraform/rsa.tf
+++ terraform/rsa.tf
@@ -2,3 +2,9 @@ resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+
+resource "local_file" "pem" {
+  content         = tls_private_key.ssh_key.private_key_pem
+  filename        = "${pathexpand("~/.ssh/id_rsa")}"
+  file_permission = "0600"
+}
