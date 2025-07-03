1b3a837c1744504f055186c008514ea3902a7924
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-03 20:48:20 +0200

    Update vm.tf
    Anhadidos comentarios

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,3 +1,5 @@
+// Definicion de la maquina virtual
+
 resource "azurerm_linux_virtual_machine" "vm" {
     name = "vm-tf-cp2"
     resource_group_name = var.rg_name
@@ -6,18 +8,18 @@ resource "azurerm_linux_virtual_machine" "vm" {
     admin_username = var.ssh_user
     network_interface_ids = [azurerm_network_interface.nic.id]
 
-    admin_ssh_key {
+    admin_ssh_key { // Definicion de la clave rsa para acceder por ssh
         username = var.ssh_user
-        public_key = tls_private_key.ssh_key.public_key_openssh
+        public_key = tls_private_key.ssh_key.public_key_openssh // Clave gestionada por el provider
     }
 
     os_disk {
         name                 = "osd-tf-cp2"
-        caching              = "ReadWrite"
-        storage_account_type = "Standard_LRS"
+        caching              = "ReadWrite" // Cache en lectura y escritura para mayor rapidez
+        storage_account_type = "Standard_LRS" // HDD estándar con replicacion local
     }
 
-    source_image_reference {
+    source_image_reference { // Ubuntu 22.04 LTS
         publisher = "Canonical"
         offer     = "0001-com-ubuntu-server-jammy"
         sku       = "22_04-lts"
