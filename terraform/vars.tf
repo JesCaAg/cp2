1222bd9588dd35bd757429a5af5882eb689ade8b
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-03 12:50:37 +0200

    Update vars.tf
    Creacion de las variables minimas obligatorias para la realizacion del cp2

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,1 +1,17 @@
+variable "location" {
+  type = string
+  description = "Region de Azure donde crearemos la infraestructura"
+  default = "West Europe" 
+}
 
+variable "public_key_path" {
+  type = string
+  description = "Ruta para la clave pública rsa para acceder a las vm"
+  default = "~/.ssh/id_rsa.pub"
+}
+
+variable "ssh_user" {
+  type = string
+  description = "Usuario utilizado para acceder por ssh"
+  default = "azureuser"
+}
