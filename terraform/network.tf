a434c618bc9de84c7b2323705a70fc8fde4d9f7b
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-03 20:57:05 +0200

    Update network.tf
    Anhadidos comentarios

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,11 +1,13 @@
+// Definicion de la ip publica
 resource "azurerm_public_ip" "pip" {
   name                = "pip-tf-cp2"
   location            = var.location
   resource_group_name = var.rg_name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
+  allocation_method   = "Dynamic" // Ip dinamica
+  sku                 = "Basic" // Ip basica
 }
 
+// Definicion de la red virtual
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-tf-cp2"
   location            = var.location
@@ -13,22 +15,24 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
 }
 
+// Definicion de la subred
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-tf-cp2"
   resource_group_name  = var.rg_name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name // Asociacion con la red virtual
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+// Definicion del interfaz de red
 resource "azurerm_network_interface" "nic" {
   name                = "nic-tf-cp2"
   location            = var.location
   resource_group_name = var.rg_name
 
-    ip_configuration {
+    ip_configuration { // Asociacion del nic con la subred y la ip publica
         name                          = "internal"
         subnet_id                     = azurerm_subnet.subnet.id
-        private_ip_address_allocation = "Dynamic"
+        private_ip_address_allocation = "Dynamic" // Asignacion automatica
         public_ip_address_id          = azurerm_public_ip.pip.id
     }
 }
