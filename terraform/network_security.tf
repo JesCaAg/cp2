96c98e7e3fc34eeed97388ade87471ea12eea94a
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-03 20:35:44 +0200

    Update network_security.tf
    Definicion del network security group y asociacion con el nic

--- terraform/network_security.tf
+++ terraform/network_security.tf
@@ -1,1 +1,10 @@
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-tf-cp2"
+  location            = var.location
+  resource_group_name = var.rg_name
+}
 
+resource "azurerm_network_interface_security_group_association" "nisga" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
