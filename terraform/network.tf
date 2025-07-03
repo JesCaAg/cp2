d2a09da28601b83349aee5b03c8a3d5db948f99e
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-03 14:39:59 +0200

    Update network.tf
    Definicion de la ip publica, la virtual network, subred y nic

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,1 +1,34 @@
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-tf-cp2"
+  location            = var.location
+  resource_group_name = var.rg_name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-tf-cp2"
+  location            = var.location
+  resource_group_name = var.rg_name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-tf-cp2"
+  resource_group_name  = var.rg_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-tf-cp2"
+  location            = var.location
+  resource_group_name = var.rg_name
+
+    ip_configuration {
+        name                          = "internal"
+        subnet_id                     = azurerm_subnet.subnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.pip.id
+    }
+}
