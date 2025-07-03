c9a48361e0b74da1f97ea152578de62bd4f93954
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-03 21:01:02 +0200

    Update rsa.tf
    Anhadidos comentarios

--- terraform/rsa.tf
+++ terraform/rsa.tf
@@ -1,10 +1,12 @@
+// Definicion de la clave para ssh
 resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
+// Generacion y descarga de la clave privada
 resource "local_file" "pem" {
   content         = tls_private_key.ssh_key.private_key_pem
   filename        = "${pathexpand("~/.ssh/id_rsa")}"
-  file_permission = "0600"
+  file_permission = "0600" // Permiso de lectura y escritura para el propietario
 }
