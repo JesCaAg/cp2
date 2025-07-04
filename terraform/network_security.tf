a6b5445da2db4e050cd84945370ca9420153b60a
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-04 13:10:20 +0200

    Update network_security.tf
    Anhadido tag

--- terraform/network_security.tf
+++ terraform/network_security.tf
@@ -3,6 +3,10 @@ resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-tf-cp2"
   location            = var.location
   resource_group_name = var.rg_name
+  
+  tags = {
+    environment = "casopractico2"
+  }
 }
 
 // Definicion de la apertura de puertos para http
