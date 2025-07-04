e158f7a483b58f4c0f32594b264faafae42ad519
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-04 13:19:08 +0200

    Update acr.tf

--- terraform/acr.tf
+++ terraform/acr.tf
@@ -1,1 +1,12 @@
+// Definicion del acr
+resource "azurerm_container_registry" "acr" {
+  name                = "acr-tf-cp2"
+  location            = var.location
+  resource_group_name = var.rg_name
+  sku                 = "Basic" // Plan basico
+  admin_enabled       = true // Habilita cuenta de admin
 
+  tags = {
+    environment = "casopractico2"
+  }
+}
