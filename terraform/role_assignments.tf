fc6b334116ae12f0a6f7fd8f17151dfbeff745ce
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-04 13:43:40 +0200

    Update role_assignments.tf
    Asignacion de rol para descargar imagenes del acr

--- terraform/role_assignments.tf
+++ terraform/role_assignments.tf
@@ -1,1 +1,6 @@
-
+// Asignacion de roles para descargar imagenes del acr para desplegar contenedores
+resource "azurerm_role_assignment" "aks-acr" {
+  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id // 
+  role_definition_name = "AcrPull" // Permiso para hacer pull
+  scope                = azurerm_container_registry.acr.id // Alcance (acr)
+}
