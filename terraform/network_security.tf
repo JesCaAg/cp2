f5eb3efe7c956d9883a7f15c88d1306a3e42263e
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-06 09:07:49 +0200

    Update network_security.tf
    Cambio de rg y location para solucionar problemas de dependencias

--- terraform/network_security.tf
+++ terraform/network_security.tf
@@ -1,8 +1,8 @@
 // Definicion del security group
 resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-tf-cp2"
-  location            = var.location
-  resource_group_name = var.rg_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   
   tags = {
     environment = "casopractico2"
