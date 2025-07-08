d28a6927a75bd84ef51fb3caa367966473604f87
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-08 19:38:40 +0200

    Update aks.tf
    Cambio comentario dns_prefix

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -3,7 +3,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks-tf-cp2"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "aks-tf-cp2-jca" // Prefijo dns para el endpoint publico
+  dns_prefix          = "aks-tf-cp2-jca" // Prefijo dns para el acceso al aks
   sku_tier            = "Standard" // Plan escogido
 
   default_node_pool {
