354eaeaca9b5ede115a42d0d31d7abf4059de6b7
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-09 13:28:36 +0200

    Update outputs.tf
    Actualizacion con nuevos outputs y creadas dependencias para sacar la ip publica en ejecucion (salia vacia)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,14 @@
 // Salida de la ip publica de la vm
 output "pip_vm" {
-  value = azurerm_public_ip.pip.ip_address
+  value = data.azurerm_public_ip.pip.ip_address
+}
+
+// Salida de la info de login del acr
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+// Salida del nombre del cluster de aks
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
 }
