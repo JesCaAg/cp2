9b4656a09f0dc83eb17e684e37f6cce609f33c1c
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-09 14:20:57 +0200

    Update outputs.tf
    Anhadidos outputs con usuario y pass de acr

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,6 +8,17 @@ output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
 
+// Salida del usuario admin de acr
+output "acr_user" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+// Salida del pass de admin de acr
+output "acr_pass" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
+
 // Salida del nombre del cluster de aks
 output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.aks.name
