b96e00aba05ec45de77ff87d3c8640df9c76ddad
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-17 20:25:33 +0200

    Update rsa.tf
    Anhadida clave publica

--- terraform/rsa.tf
+++ terraform/rsa.tf
@@ -10,3 +10,10 @@ resource "local_file" "pem" {
   filename        = "${pathexpand("~/.ssh/id_rsa")}"
   file_permission = "0600" // Permiso de lectura y escritura para el propietario
 }
+
+// Generacion y descarga de la clave publica
+resource "local_file" "public_key" {
+  content         = tls_private_key.ssh_key.public_key_openssh
+  filename        = "${pathexpand("~/.ssh/id_rsa.pub")}"
+  file_permission = "0644"
+}
