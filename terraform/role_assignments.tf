431e63632cc519fb32b93ec14d2b2d122d225fcf
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-19 14:45:18 +0200

    Update role_assignments.tf
    Creadas dependencias

--- terraform/role_assignments.tf
+++ terraform/role_assignments.tf
@@ -3,4 +3,9 @@ resource "azurerm_role_assignment" "aks-acr" {
   principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id // Quien recibe el permiso (aks)
   role_definition_name = "AcrPull" // Permiso para hacer pull
   scope                = azurerm_container_registry.acr.id // Alcance (acr)
+
+  depends_on = [
+    azurerm_kubernetes_cluster.aks,
+    azurerm_container_registry.acr
+  ]
 }
