003daf411c0acf4bfa8afeff64df0a113b614848
JesCaAg <jesus.carral133@comunidadunir.net>
2025-07-21 19:21:25 +0200

    Update role_assignments.tf
    Cambio en el principal id

--- terraform/role_assignments.tf
+++ terraform/role_assignments.tf
@@ -1,8 +1,9 @@
 // Asignacion de roles para descargar imagenes del acr para desplegar contenedores
 resource "azurerm_role_assignment" "aks-acr" {
-  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id // Quien recibe el permiso (aks)
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id // Quien recibe el permiso (aks - kubelet)
   role_definition_name = "AcrPull" // Permiso para hacer pull
   scope                = azurerm_container_registry.acr.id // Alcance (acr)
+  skip_service_principal_aad_check = true
 
   depends_on = [
     azurerm_kubernetes_cluster.aks,
